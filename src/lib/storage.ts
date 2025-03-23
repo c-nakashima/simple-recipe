87d557c9a61698e93638794b46ea7bf6b62ac3a5
c-nakashima <chisatonakashima1025@gmail.com>
2025-03-24 09:40:32 +1100

    add: add client side only

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -8,11 +8,19 @@ export type Recipe = {
 
 // get recipes
 export const getRecipes = (): Recipe[] => {
+  // Run only client side
+  if (typeof window === "undefined") {
+    return [];
+  }
   return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
 };
 
 // save recipes
 export const saveRecipe = (recipe: Recipe) => {
+  // Run only client side
+  if (typeof window === "undefined") {
+    return;
+  }
   const recipes = getRecipes();
   recipes.push(recipe);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(recipes));
@@ -20,12 +28,19 @@ export const saveRecipe = (recipe: Recipe) => {
 
 // delete recipes
 export const deleteRecipe = (id: number) => {
+  // Run only client side
+  if (typeof window === "undefined") {
+    return;
+  }
   const recipes = getRecipes().filter((_, index) => index !== id);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(recipes));
 };
 
 // update recipes
 export const updateRecipe = (id: number, updatedRecipe: Recipe) => {
+  if (typeof window === "undefined") {
+    return;
+  }
   const recipes = getRecipes();
   recipes[id] = updatedRecipe;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(recipes));
